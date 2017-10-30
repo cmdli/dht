9a09423cbf92b5e18d18eecb44b105bb76347cd6
Chris de la Iglesia <christopher.delaiglesia@gmail.com>
2017-10-30 11:59:35 -0700

    Add logging to TestServer

--- src/main/java/com/cmdli/dht/TestServer.java
+++ src/main/java/com/cmdli/dht/TestServer.java
@@ -16,10 +16,11 @@ public class TestServer {
             System.out.printf("Started server at %s:%d\n", InetAddress.getLocalHost(), serverSocket.getLocalPort());
             while (true) {
                 Socket clientSocket = serverSocket.accept();
+                System.out.printf("New connection from %s:%d\n", clientSocket.getInetAddress(), clientSocket.getPort());
                 BufferedReader networkIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String inputLine = null;
                 while ((inputLine = networkIn.readLine()) != null) {
-                    System.out.println(inputLine);
+                    System.out.printf("Recieved: %s\n", inputLine);
                 }
                 networkIn.close();
                 clientSocket.close();
